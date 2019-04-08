d5a9a2c46441b0321ee985ac91a4bd5f068e0a30
talha3111997 <talha3111997@gmail.com>
2019-04-09 01:31:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/muhammadtalha/dictionary/MainActivity.java
@@ -0,0 +1,289 @@
+package com.example.muhammadtalha.dictionary;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Scanner;
+
+import static android.icu.lang.UCharacter.toLowerCase;
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class MainActivity extends AppCompatActivity{
+
+    private static final String TAG = "MainActivity";
+
+    private TextView mTextMessage;
+    private Button mButton;
+    private EditText mEditText;
+    int flag = 0;
+    Map1<String, String> map = new Map1<>();
+    String key,value;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  mTextMessage = (TextView) findViewById(R.id.message);
+       // BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+      //  navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search();
+            }
+        });
+
+        mEditText = findViewById(R.id.edittext);
+        mEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formMap();
+            }
+        });
+
+    }
+        private void formMap(){
+         if(flag == 0){
+            try {
+
+                InputStream inputStream = getAssets().open("Dictionary.txt");
+                //int size = inputStream.available();
+                // Log.d("SIZEOFFILE", "size of file is: " + size);
+                Scanner scanner = new Scanner(inputStream);
+                while(scanner.hasNext()){
+                    key = toLowerCase(scanner.next());
+                    // Log.d("WORDS", "WORD: " + key);
+                    value = scanner.nextLine();
+                    map.add(key,value);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        flag++;}
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void search(){
+
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        //inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+        EditText search = findViewById(R.id.edittext);
+        String key1 = search.getText().toString().trim();
+        Log.d(TAG, "search: ");
+        key1 = toLowerCase(key1);
+        String value1 = map.get(key1);
+        TextView meaning= findViewById(R.id.meaning);
+        if (value1 == null){
+        meaning.setText("SORRY, WORD NOT FOUND");
+          } else {
+            meaning.setText(value1.toString());
+          }
+
+    }
+    
+    
+
+
+
+
+
+
+}
+class HashNode<K, V>
+{
+    K key;
+    V value;
+    
+    // Reference to next node
+    HashNode<K, V> next;
+
+    // Constructor
+    public HashNode(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
+class Map1<K, V>
+{
+    // bucketArray is used to store array of chains
+    private ArrayList<HashNode<K, V>> bucketArray;
+
+    // Current capacity of array list
+    private int numBuckets;
+
+    // Current size of array list
+    private int size;
+
+    // Constructor (Initializes capacity, size and
+    // empty chains.
+    public Map1()
+    {
+        bucketArray = new ArrayList<>();
+        numBuckets = 1000;
+        size = 0;
+
+        // Create empty chains
+        for (int i = 0; i < numBuckets; i++)
+            bucketArray.add(null);
+    }
+
+    public int size() { return size; }
+    public boolean isEmpty() { return size() == 0; }
+
+    // This implements hash function to find index
+    // for a key
+    private int getBucketIndex(K key)
+    {
+        int hashCode = key.hashCode();
+        if(hashCode<0)
+            hashCode *= -1;
+        int index = hashCode % numBuckets;
+        return index;
+    }
+
+    // Method to remove a given key
+    public V remove(K key)
+    {
+        // Apply hash function to find index for given key
+        int bucketIndex = getBucketIndex(key);
+
+        // Get head of chain
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        // Search for key in its chain
+        HashNode<K, V> prev = null;
+        while (head != null)
+        {
+            // If Key found
+            if (head.key.equals(key))
+                break;
+
+            // Else keep moving in chain
+            prev = head;
+            head = head.next;
+        }
+
+        // If key was not there
+        if (head == null)
+            return null;
+
+        // Reduce size
+        size--;
+
+        // Remove key
+        if (prev != null)
+            prev.next = head.next;
+        else
+            bucketArray.set(bucketIndex, head.next);
+
+        return head.value;
+    }
+
+    // Returns value for a key
+    public V get(K key)
+    {
+        // Find head of chain for given key
+        int bucketIndex = getBucketIndex(key);
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        // Search key in chain
+        while (head != null)
+        {
+            if (head.key.equals(key))
+                return head.value;
+            head = head.next;
+        }
+
+        // If key not found
+        return null;
+    }
+
+    // Adds a key value pair to hash
+    public void add(K key, V value)
+    {
+        // Find head of chain for given key
+        int bucketIndex = getBucketIndex(key);
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        // Check if key is already present
+        while (head != null)
+        {
+            if (head.key.equals(key))
+            {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+        // Insert key in chain
+        size++;
+        head = bucketArray.get(bucketIndex);
+        HashNode<K, V> newNode = new HashNode<K, V>(key, value);
+        newNode.next = head;
+        bucketArray.set(bucketIndex, newNode);
+
+
+        if ((1.0*size)/numBuckets >= 0.7)
+        {
+            ArrayList<HashNode<K, V>> temp = bucketArray;
+            bucketArray = new ArrayList<>();
+            numBuckets = 2 * numBuckets;
+            size = 0;
+            for (int i = 0; i < numBuckets; i++)
+                bucketArray.add(null);
+
+            for (HashNode<K, V> headNode : temp)
+            {
+                while (headNode != null)
+                {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+    }
+}
