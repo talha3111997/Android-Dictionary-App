1718d0f0fa8964f31582619ebdbefa979c342cd9
Muhammad Talha Umair Alim <talha3111997@gmail.com>
2019-05-15 23:34:30 +0500

    Removed unneeded comments

--- app/src/main/java/com/example/muhammadtalha/dictionary/MainActivity.java
+++ app/src/main/java/com/example/muhammadtalha/dictionary/MainActivity.java
@@ -42,9 +42,7 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      //  mTextMessage = (TextView) findViewById(R.id.message);
-       // BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-      //  navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+      
         mButton = findViewById(R.id.button);
 
         mButton.setOnClickListener(new View.OnClickListener() {
